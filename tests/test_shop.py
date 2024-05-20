e6d1d5da81336681e93826445bc843aa8964a273
Nikita Shugaley <shugaley.nk@ispras.ru>
2024-05-20 23:10:01 +0300

    Add test add_book and unknown cmd

---
+++ tests/test_shop.py
@@ -0,0 +1,16 @@
+import pytest
+from shop import Shop
+
+@pytest.fixture
+def shop():
+    return Shop()
+
+def test_unknown_command(shop):
+    res = shop.process("Неизвестная комманда")
+    assert(res == "Неизвестная команда: 'Неизвестная комманда'\n")
+
+def test_add_catalog(shop):
+    res = shop.process("Добавить книгу "
+                       "Название Автор 2000 1250 Издательство Жанр")
+    assert(res == "Книга Название Автор 2000 1250 Издательство Жанр "
+                  "добавлена\n")
