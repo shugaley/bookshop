c33ba93280063be9ebae438d0675dc1864bc6383
Nikita Shugaley <shugaley.nk@ispras.ru>
2024-05-21 00:29:50 +0300

    Add show_catalog

--- shop/__init__.py
+++ shop/__init__.py
@@ -20,29 +20,33 @@ class Shop:
         self.catalog: list[Book] = []
 
     def process(self, cmd: str) -> str:
-        '''Process command and return command's status report message'''
+        '''Process command'''
         msg = f"Неизвестная команда: '{cmd}'\n"
 
         if cmd.startswith("Добавить книгу"):
             self.__add_book(cmd.split("Добавить книгу")[1])
             new_book = self.catalog[-1]
             msg = "Книга "
-            msg += self.__transform_book_to_msg(new_book)
-            msg += "добавлена\n"
+            msg += self.__transform_book_to_str(new_book)
+            msg += " добавлена\n"
 
         elif cmd.startswith("Удалить книгу"):
             old_book = self.__remove_book(cmd.split("Удалить книгу")[1])
             msg = "Книга "
-            msg += self.__transform_book_to_msg(old_book)
-            msg += "удалена\n"
+            msg += self.__transform_book_to_str(old_book)
+            msg += " удалена\n"
+
+        elif cmd.startswith("Показать каталог"):
+            msg = "Каталог:\n"
+            msg += self.__show_catalog()
 
         return msg
 
-    def __transform_book_to_msg(self, book: Book) -> str:
+    def __transform_book_to_str(self, book: Book) -> str:
         '''Transform Book to str'''
-        msg = f"{book.title} {book.author} {book.year} "
-        msg += f"{book.price} {book.publisher} {book.genre} "
-        return msg
+        ret = f"{book.title} {book.author} {book.year} "
+        ret += f"{book.price} {book.publisher} {book.genre}"
+        return ret
 
     def __add_book(self, book_info: str) -> bool:
         '''Add book to catalog'''
@@ -58,3 +62,11 @@ class Shop:
             Book(book[0], book[1], book[2], book[3], book[4], book[5])
         self.catalog.remove(book_entity)
         return book_entity
+
+    def __show_catalog(self) -> str:
+        '''Return full current catalog'''
+        ret = ""
+        for i_book in self.catalog:
+            ret += self.__transform_book_to_str(i_book)
+            ret += "\n"
+        return ret
