55a41053694a4d7ace302771b20b01ab85470e0d
Nikita Shugaley <shugaley.nk@ispras.ru>
2024-05-21 04:28:05 +0300

    Add test add_to_cart and remove_from_cart

--- tests/test_shop.py
+++ tests/test_shop.py
@@ -5,29 +5,38 @@ from shop import Shop
 def shop():
     return Shop()
 
+default_book = "Название Автор 2000 1000 Издательство Жанр"
+
 def test_unknown_command(shop):
     res = shop.process("Неизвестная комманда")
     assert(res == "Неизвестная команда: 'Неизвестная комманда'\n")
 
-def test_add_catalog(shop):
-    res = shop.process("Добавить книгу "
-                       "Название Автор 2000 1250 Издательство Жанр")
-    assert(res == "Книга Название Автор 2000 1250 Издательство Жанр "
-                  "добавлена\n")
+def test_add_to_catalog(shop):
+    res = shop.process(f"Добавить книгу {default_book}")
+    assert(res == f"Книга {default_book} добавлена\n")
 
-def test_remove_catalog(shop):
-    shop.process("Добавить книгу Название Автор 2000 1250 Издательство Жанр")
-    res = shop.process("Удалить книгу "
-                       "Название Автор 2000 1250 Издательство Жанр")
-    assert(res == "Книга Название Автор 2000 1250 Издательство Жанр "
-                  "удалена\n")
+def test_remove_from_catalog(shop):
+    shop.process(f"Добавить книгу {default_book}")
+    res = shop.process(f"Удалить книгу {default_book}")
+    assert(res == f"Книга {default_book} удалена\n")
 
 def test_show_catalog(shop):
-    shop.process("Добавить книгу Название1 Автор1 2000 1250 Издательство Жанр")
-    shop.process("Добавить книгу Название2 Автор2 2000 1250 Издательство Жанр")
-    shop.process("Удалить книгу Название2 Автор2 2000 1250 Издательство Жанр")
-    shop.process("Добавить книгу Название3 Автор3 2000 1250 Издательство Жанр")
+    shop.process("Добавить книгу Название1 Автор1 2000 1000 Издательство Жанр")
+    shop.process("Добавить книгу Название2 Автор2 2000 1000 Издательство Жанр")
+    shop.process("Удалить книгу Название2 Автор2 2000 1000 Издательство Жанр")
+    shop.process("Добавить книгу Название3 Автор3 2000 1000 Издательство Жанр")
     res = shop.process("Показать каталог")
     assert(res == "Каталог:\n"
-                  "Название1 Автор1 2000 1250 Издательство Жанр\n"
-                  "Название3 Автор3 2000 1250 Издательство Жанр\n")
+                  "Название1 Автор1 2000 1000 Издательство Жанр\n"
+                  "Название3 Автор3 2000 1000 Издательство Жанр\n")
+
+def test_add_to_cart(shop):
+    shop.process(f"Добавить книгу {default_book}")
+    res = shop.process("Добавить в корзину Название")
+    assert(res == f"Книга {default_book} добавлена в корзину")
+
+def test_remove_from_cart(shop):
+    shop.process(f"Добавить книгу {default_book}")
+    shop.process("Добавить в корзину Название")
+    res = shop.process(f"Удалить из корзины Название")
+    assert(res == f"Книга {default_book} удалена из корзины")
