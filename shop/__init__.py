b3904c8868b2cd65c4d8c933a7dcf63ce7c1b43d
Nikita Shugaley <shugaley.nk@ispras.ru>
2024-05-21 04:35:37 +0300

    Add add_book_to_cart and remove_book_from_cart

--- shop/__init__.py
+++ shop/__init__.py
@@ -18,6 +18,7 @@ class Shop:
     '''Shop worker'''
     def __init__(self) -> None:
         self.catalog: list[Book] = []
+        self.cart: list[Book] = []
 
     def process(self, cmd: str) -> str:
         '''Process command'''
@@ -40,6 +41,20 @@ class Shop:
             msg = "Каталог:\n"
             msg += self.__show_catalog()
 
+        elif cmd.startswith("Добавить в корзину"):
+            new_book_cart = self.__add_book_to_cart(
+                cmd.split("Добавить в корзину ")[1])
+            msg = "Книга "
+            msg += self.__transform_book_to_str(new_book_cart)
+            msg += " добавлена в корзину"
+
+        elif cmd.startswith("Удалить из корзины"):
+            old_book_cart = self.__remove_book_from_cart(
+                cmd.split("Удалить из корзины ")[1])
+            msg = "Книга "
+            msg += self.__transform_book_to_str(old_book_cart)
+            msg += " удалена из корзины"
+
         return msg
 
     def __transform_book_to_str(self, book: Book) -> str:
@@ -47,6 +62,15 @@ class Shop:
         ret = f"{book.title} {book.author} {book.year} "
         ret += f"{book.price} {book.publisher} {book.genre}"
         return ret
+
+    def __find_book_by_title(self, book_title: str, lst: list[Book]) -> Book:
+        '''Find book in catalog or cart by title'''
+        book = Book("", "", "", "", "", "")
+        for i_book in lst:
+            if i_book.title == book_title:
+                book = i_book
+                break
+        return book
 
     def __add_book(self, book_info: str) -> bool:
         '''Add book to catalog'''
@@ -70,3 +94,15 @@ class Shop:
             ret += self.__transform_book_to_str(i_book)
             ret += "\n"
         return ret
+
+    def __add_book_to_cart(self, book_title: str) -> Book:
+        '''Add book to cart'''
+        book = self.__find_book_by_title(book_title, self.catalog)
+        self.cart.append(book)
+        return book
+
+    def __remove_book_from_cart(self, book_title: str) -> Book:
+        '''Remove book from cart'''
+        book = self.__find_book_by_title(book_title, self.cart)
+        self.cart.remove(book)
+        return book
